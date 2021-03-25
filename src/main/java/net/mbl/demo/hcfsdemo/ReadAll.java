9865cf11d8834e217bb57ab30c91a3f826486bbf
maobaolong <maobaolong@139.com>
2021-03-25 15:58:16 +0800

    Add a Read all class to read all files.

---
+++ src/main/java/net/mbl/demo/hcfsdemo/ReadAll.java
@@ -0,0 +1,38 @@
+package net.mbl.demo.hcfsdemo;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.NullOutputStream;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+
+public class ReadAll {
+
+  public static void readAllFiles(FileSystem fs, FileStatus parentStatus) throws IOException {
+
+    FileStatus[] fileStatuses = fs.listStatus(parentStatus.getPath());
+    for(FileStatus status : fileStatuses) {
+      if (status.isDirectory()) {
+        readAllFiles(fs, status);
+      } else {
+        try (FSDataInputStream is = fs.open(status.getPath())) {
+          System.out.println("start read " + status.getPath());
+          IOUtils.copy(is, new NullOutputStream(), 4096);
+          System.out.println("finish read " + status.getPath());
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Path path = new Path(args[0]);
+    FileSystem fs = path.getFileSystem(conf);
+    FileStatus status = fs.getFileStatus(path);
+    readAllFiles(fs, status);
+  }
+}
